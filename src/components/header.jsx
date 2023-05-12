31a0dd929e5c8705482d2183d40f46473768337a
thais de rodez <thaisderodez@gmail.com>
2023-05-12 17:53:03 +0200

    fix(header): navlink

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,30 +1,37 @@
+import { useSelector } from 'react-redux';
 import logo from '../img/argentBankLogo.png';
 import '../styles/components/header.css';
+import { Link } from 'react-router-dom';
 
 export function Header() {
+  const user = useSelector((state) => state.user.data);
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <Link to="/" className="main-nav-logo">
         <img
           className="main-nav-logo-image"
           src={logo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
       <div>
-        <a className="main-nav-item" href="./user.html">
-          <i className="fa fa-user-circle"></i>
-          Tony
-        </a>
-        <a className="main-nav-item" href="./sign-in.html">
+        {user && (
+          <>
+            <Link to="profile" className="main-nav-item" href="./user.html">
+              <i className="fa fa-user-circle"></i>
+              Tony
+            </Link>
+            <Link to="/" className="main-nav-item">
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </Link>
+          </>
+        )}
+        <Link to="login" className="main-nav-item">
           <i className="fa fa-user-circle"></i>
           Sign In
-        </a>
-        <a className="main-nav-item" href="./index.html">
-          <i className="fa fa-sign-out"></i>
-          Sign Out
-        </a>
+        </Link>
       </div>
     </nav>
   );
