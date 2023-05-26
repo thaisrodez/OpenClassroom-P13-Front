c9289fc45e649e1b09b036723a72491d2d28182d
thais de rodez <thaisderodez@gmail.com>
2023-05-26 10:18:58 +0200

    fix(header): get user data and handle logout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,19 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import logo from '../img/argentBankLogo.png';
 import '../styles/components/header.css';
 import { Link } from 'react-router-dom';
+import { removeUser } from '../features/user';
+import Auth from '../services/auth';
 
 export function Header() {
-  const user = useSelector((state) => state.user.data);
+  const dispatch = useDispatch();
+  const firstName = useSelector((state) => state.user.firstName);
+
+  const handleLogout = () => {
+    Auth.logout();
+    dispatch(removeUser());
+  };
+
   return (
     <nav className="main-nav">
       <Link to="/" className="main-nav-logo">
@@ -16,22 +25,20 @@ export function Header() {
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
       <div>
-        {user && (
+        {firstName ? (
           <>
             <Link to="profile" className="main-nav-item" href="./user.html">
-              <i className="fa fa-user-circle"></i>
-              Tony
+              <i className="fa fa-user-circle"></i> {firstName}
             </Link>
-            <Link to="/" className="main-nav-item">
-              <i className="fa fa-sign-out"></i>
-              Sign Out
+            <Link to="/" className="main-nav-item" onClick={handleLogout}>
+              <i className="fa fa-sign-out"></i> Sign Out
             </Link>
           </>
+        ) : (
+          <Link to="login" className="main-nav-item">
+            <i className="fa fa-user-circle"></i> Sign In
+          </Link>
         )}
-        <Link to="login" className="main-nav-item">
-          <i className="fa fa-user-circle"></i>
-          Sign In
-        </Link>
       </div>
     </nav>
   );
